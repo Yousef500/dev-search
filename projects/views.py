48e550ce0e1bc45b074ed60eece081b86e297b67
Yousef500 <Kazuya14725@gmail.com>
2021-09-03 22:03:59 +0300

    Finished 3rd

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Project
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    return render(request, 'projects/single-project.html', {'project': project, 'tags': tags})
